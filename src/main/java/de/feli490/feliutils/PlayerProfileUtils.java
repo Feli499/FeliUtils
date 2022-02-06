f77c6e57e2f59681b6ef84cb1193299f5fda22d5
Maik E <maik@ehemanns.de>
2022-02-06 19:40:28 +0100

    Removed a try Catch inside of the PlayerProfileUtils, it was implemented while debugging and never got removed. Also Fixed a wrong import

--- src/main/java/de/feli490/feliutils/PlayerProfileUtils.java
+++ src/main/java/de/feli490/feliutils/PlayerProfileUtils.java
@@ -3,7 +3,7 @@ package de.feli490.feliutils;
 import java.nio.charset.StandardCharsets;
 import java.util.Set;
 
-import org.json.JSONObject;
+import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
 import com.destroystokyo.paper.profile.PlayerProfile;
@@ -16,15 +16,15 @@ public class PlayerProfileUtils {
         throw new Error("nope.");
     }
 
-    public static String getSkinURLFromPlayerProfile(PlayerProfile profile) {
+    public static String getSkinURLFromPlayerProfile(PlayerProfile profile) throws Exception {
         return profile == null ? null : getSkinURLFromProfilePropertySet(profile.getProperties());
     }
 
-    public static String getSkinURLFromCachedPlayerProfile(CachedPlayerProfile profile) {
+    public static String getSkinURLFromCachedPlayerProfile(CachedPlayerProfile profile) throws Exception {
         return profile == null ? null : getSkinURLFromProfilePropertySet(profile.getProperties());
     }
 
-    public static String getSkinURLFromProfilePropertySet(Set<ProfileProperty> properties) {
+    public static String getSkinURLFromProfilePropertySet(Set<ProfileProperty> properties) throws Exception {
 
         String profilePropertyValue = ermittleTexturePropertyValue(properties);
         return profilePropertyValue == null ? null : getSkinURLFromTexturesProperty(profilePropertyValue);
@@ -39,20 +39,17 @@ public class PlayerProfileUtils {
         return null;
     }
 
-    public static String getSkinURLFromTexturesProperty(String value) {
-        try {
-            String decodedValue = new String(java.util.Base64.getDecoder().decode(value), StandardCharsets.UTF_8);
-            if (decodedValue.startsWith("{textures:{SKIN:{url:")) { // fix old broken json
-                decodedValue = decodedValue.replace("{textures:{SKIN:{url:", "{\"textures\":{\"SKIN\":{\"url\":");
-            }
-            JSONObject json = (JSONObject) new JSONParser().parse(decodedValue);
-            JSONObject jsonTextures = (JSONObject) json.get("textures");
-            JSONObject jsonSkin = (JSONObject) jsonTextures.get("SKIN");
-            return (String) jsonSkin.get("url");
-        } catch (Exception e2) {
-            e2.printStackTrace();
+    public static String getSkinURLFromTexturesProperty(String value) throws Exception {
+
+        String decodedValue = new String(java.util.Base64.getDecoder().decode(value), StandardCharsets.UTF_8);
+        if (decodedValue.startsWith("{textures:{SKIN:{url:")) { // fix old broken json
+            decodedValue = decodedValue.replace("{textures:{SKIN:{url:", "{\"textures\":{\"SKIN\":{\"url\":");
         }
-        return null;
+
+        JSONObject json = (JSONObject) new JSONParser().parse(decodedValue);
+        JSONObject jsonTextures = (JSONObject) json.get("textures");
+        JSONObject jsonSkin = (JSONObject) jsonTextures.get("SKIN");
+        return (String) jsonSkin.get("url");
     }
 
     public static String getTexturesProperyFromSkinURL(String url) {
