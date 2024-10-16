fd6d18fd7e345693a80281746757a5b840adc0c6
Feli <maik@ehemanns.de>
2024-10-17 01:27:19 +0200

    Fixed odd entity behavior

--- src/main/java/de/feli490/feliutils/entities/MultipleEntityUnit.java
+++ src/main/java/de/feli490/feliutils/entities/MultipleEntityUnit.java
@@ -39,7 +39,6 @@ public class MultipleEntityUnit {
     public void positionEntity(Location location, float yaw) {
         Vector vector = MultipleEntityUtils.rotateVector(this.position, yaw);
         Location add = location.clone().add(vector);
-        add.setYaw(this.baseYaw + yaw);
         add.setPitch(this.basePitch);
         this.entity.teleport(add);
     }
