8650977fe7b3516599bd3155f7ecbb11990c6bb7
Feli <maik@ehemanns.de>
2026-01-18 14:12:26 +0100

    Removed MultiModule structure. Core and Hytale are now seperrate Repos again

--- FeliUtilsCore/src/main/java/de/feli490/utils/core/search/ExpressionParser.java
+++
@@ -1,9 +0,0 @@
-package de.feli490.utils.core.search;
-
-import de.feli490.utils.core.search.exceptions.ParseException;
-import de.feli490.utils.core.search.query.expressions.Expression;
-
-public interface ExpressionParser {
-
-    Expression parse(StringIterator stringIterator) throws ParseException;
-}
--- FeliUtilsCore/src/main/java/de/feli490/utils/core/search/query/expressions/Expression.java
+++
@@ -1,9 +0,0 @@
-package de.feli490.utils.core.search.query.expressions;
-
-import de.feli490.utils.core.search.ParameterValueContainer;
-
-public interface Expression {
-
-    boolean isTrue(ParameterValueContainer parameterValueContainer);
-
-}
